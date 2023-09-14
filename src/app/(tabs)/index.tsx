b60a20d73583162eb0ff3a38c93c32e0620bcfde
Vadim Savin <savinvadim1312@gmail.com>
2023-09-14 16:19:56 +0100

    Type sage components

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,6 +1,6 @@
 import { View } from 'react-native';
-import products from '../../../assets/data/products';
-import ProductListItem from '../../components/ProductListItem';
+import products from '@assets/data/products';
+import ProductListItem from '@components/ProductListItem';
 
 export default function MenuScreen() {
   return (
--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -1,10 +1,21 @@
 import { StyleSheet, Text, View, Image } from 'react-native';
 import Colors from '../constants/Colors';
+import { Product } from '../types';
 
-const ProductListItem = ({ product }) => {
+export const defaultPizzaImage =
+  'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png';
+
+type ProductListItemProps = {
+  product: Product;
+};
+
+const ProductListItem = ({ product }: ProductListItemProps) => {
   return (
     <View style={styles.container}>
-      <Image source={{ uri: product.image }} style={styles.image} />
+      <Image
+        source={{ uri: product.image || defaultPizzaImage }}
+        style={styles.image}
+      />
 
       <Text style={styles.title}>{product.name}</Text>
       <Text style={styles.price}>${product.price}</Text>
