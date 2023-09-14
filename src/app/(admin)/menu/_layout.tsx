b4bc2efe18e077bd9ebf1acd49fb3e1fa5aa857f
Vadim Savin <savinvadim1312@gmail.com>
2023-09-14 19:28:20 +0100

    Create a product

--- src/app/(admin)/menu/_layout.tsx
+++ src/app/(admin)/menu/_layout.tsx
@@ -11,7 +11,7 @@ export default function MenuStack() {
         options={{
           title: 'Menu',
           headerRight: () => (
-            <Link href="/" asChild>
+            <Link href="/(admin)/menu/create" asChild>
               <Pressable>
                 {({ pressed }) => (
                   <FontAwesome
---
+++ src/app/(admin)/menu/create.tsx
@@ -0,0 +1,129 @@
+import Button from '@/components/Button';
+import { defaultPizzaImage } from '@/components/ProductListItem';
+import Colors from '@/constants/Colors';
+import { useState } from 'react';
+import { View, Text, StyleSheet, TextInput, Image } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import { Stack } from 'expo-router';
+
+const CreateProductScreen = () => {
+  const [name, setName] = useState('');
+  const [price, setPrice] = useState('');
+  const [errors, setErrors] = useState('');
+  const [image, setImage] = useState<string | null>(null);
+
+  const resetFields = () => {
+    setName('');
+    setPrice('');
+  };
+
+  const validateInput = () => {
+    setErrors('');
+    if (!name) {
+      setErrors('Name is required');
+      return false;
+    }
+    if (!price) {
+      setErrors('Price is required');
+      return false;
+    }
+    if (isNaN(parseFloat(price))) {
+      setErrors('Price is not a number');
+      return false;
+    }
+    return true;
+  };
+
+  const onCreate = () => {
+    if (!validateInput()) {
+      return;
+    }
+
+    console.warn('Creating product: ', name);
+
+    // Save in the database
+
+    resetFields();
+  };
+
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Stack.Screen options={{ title: 'Create Product' }} />
+
+      <Image
+        source={{ uri: image || defaultPizzaImage }}
+        style={styles.image}
+      />
+      <Text onPress={pickImage} style={styles.textButton}>
+        Select Image
+      </Text>
+
+      <Text style={styles.label}>Name</Text>
+      <TextInput
+        value={name}
+        onChangeText={setName}
+        placeholder="Name"
+        style={styles.input}
+      />
+
+      <Text style={styles.label}>Price ($)</Text>
+      <TextInput
+        value={price}
+        onChangeText={setPrice}
+        placeholder="9.99"
+        style={styles.input}
+        keyboardType="numeric"
+      />
+
+      <Text style={{ color: 'red' }}>{errors}</Text>
+      <Button onPress={onCreate} text="Create" />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 10,
+  },
+  image: {
+    width: '50%',
+    aspectRatio: 1,
+    alignSelf: 'center',
+  },
+  textButton: {
+    alignSelf: 'center',
+    fontWeight: 'bold',
+    color: Colors.light.tint,
+    marginVertical: 10,
+  },
+
+  input: {
+    backgroundColor: 'white',
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 5,
+    marginBottom: 20,
+  },
+  label: {
+    color: 'gray',
+    fontSize: 16,
+  },
+});
+
+export default CreateProductScreen;
